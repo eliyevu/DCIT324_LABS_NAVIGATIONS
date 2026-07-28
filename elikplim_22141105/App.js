df57286725cca8b51f1b6794f21248d47c73cf1b
eliyevu <kancartos6@gmail.com>
2026-07-28 00:36:31 +0000

    modified

--- elikplim_22141105/App.js
+++ elikplim_22141105/App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { createContext, useState } from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { initialProfile } from './data/data';
+import RootStack from './navigation/RootStack';
+
+
+
+export const ProfileContext = createContext({
+  profile: initialProfile,
+  updateProfile: () => {},
+});
 
 export default function App() {
+  const [profile, setProfile] = useState(initialProfile);
+
+  const updateProfile = (updates) => {
+    setProfile((prev) => ({ ...prev, ...updates }));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ProfileContext.Provider value={{ profile, updateProfile }}>
+        <NavigationContainer>
+          <RootStack />
+        </NavigationContainer>
+      </ProfileContext.Provider>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
